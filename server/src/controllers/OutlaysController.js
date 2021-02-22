e326126977aefe099a001902fe71abc3edfac390
Yagho Mattos <mattosyagho@gmail.com>
2021-02-22 16:58:04 -0300

    delete route in outlay

---
+++ server/src/controllers/OutlaysController.js
@@ -0,0 +1,43 @@
+//importe da conexão com o banco de dados
+const connection = require('../database/connection');
+
+module.exports = {
+  async index(request, response) {
+    const outlays = await connection('outlays').select('*');
+  
+    return response.json(outlays);
+  },  
+
+  async create(request, response) {
+    const { value, name, type } = request.body;
+    const user_id = request.headers.authorization;
+
+    const [id] = await connection('outlays').insert({
+      value,
+      name,
+      type,
+      user_id,
+    });
+
+    return response.json({ id });
+  },
+
+  async delete(request, response) {
+    const { id } = request.params;
+    const user_id = request.headers.authorization;
+
+    const outlay = await connection('outlays')
+      .where('id', id)
+      .select('user_id')
+      .first();
+
+    //verifica se o usuário esta tentando deletar um outlay que não seja dele mesmo
+    if(outlay.user_id !== user_id) {
+      return response.status(401).json({ error:'Operation not permitted.' });
+    }
+
+    await connection('outlays').where('id', id).delete();
+    //retorna uma reposta ok vazia
+    return response.status(204).send();
+  }
+};
--- server/src/routes.js
+++ server/src/routes.js
@@ -2,20 +2,22 @@
 const express = require('express');
 
 const UserController = require('./controllers/UserController');
-const OutlaysController = require('./controllers/OtlaysController');
+const OutlaysController = require('./controllers/OutlaysController');
 
 //instância router do express para controle de rotas
 const routes = express.Router();
 
-//rota get para ver todos users
+//rota get para ver todos users -> desenvolvimento apenas
 routes.get('/user', UserController.index);
 //rota post para criar user
 routes.post('/user', UserController.create);
 
 //rota post para criar outlay
 routes.post('/outlays', OutlaysController.create);
-//rota get para ver todos users
+//rota get listar todos outlays
 routes.get('/outlays', OutlaysController.index);
+//rota put para deletar outlay
+routes.delete('/outlays/:id', OutlaysController.delete);
 
 //exporta o routes para uso externo
 module.exports = routes;
