a21211b8f17752e0c1acebe207e666321885800b
Yagho Mattos <mattosyagho@gmail.com>
2021-02-28 14:19:42 -0300

    login page test

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 import './styles.css';
+import '../Register/index';
 
 function Login() {
   return(
-    <h1>Hello World</h1>
+    <div className="login-container">
+      <section className="form">
+        <img src="" alt="logo"/>
+        <form>
+          <h1>Fazer login</h1>
+
+          <input placeholder="login"/>
+          <input placeholder="senha"/>
+          <a className="button singing" href="/singin">Entrar</a>
+
+          <a className="button register" href="/register">Cadastrar</a>
+        </form>
+      </section>
+    </div>
   );
 }
 
