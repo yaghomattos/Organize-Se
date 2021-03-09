0cc6e2d7fc2f6dc22d17cc384e596683a8871c29
Yagho Mattos <mattosyagho@gmail.com>
2021-03-08 21:45:13 -0300

    register page conect with backend

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,19 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory}  from 'react-router-dom';
+import api from '../../services/api';
 
 import './styles.css';
 
 function Register() {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [validate, setValidate] = useState('');
+
+  const history = useHistory();
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    const data = {
+      login,
+      password,
+      name,
+      email,
+    };
+
+    try {
+      localStorage.setItem('validate', validate);
+      if(verifyPassword()) {
+        const response = await api.post('user', data);
+
+        alert(`Cadastro realizado com sucesso: ${response.data.id}`);
+
+        history.push('/');
+      }
+      else {
+        alert('Senhas diferente');
+      }  
+    } catch(err) {
+      alert('Erro no cadastro');
+    }
+  }
+
+  function verifyPassword() {
+    if(validate === password) {
+      return true;
+    }
+  }
+
   return(
     <div className="container">
       <section className="form">
         <img src="" alt="logo"/>
         <h1>Fazer Cadastro</h1>
 
-        <form>
-          <input placeholder="login"/>
-          <input placeholder="senha"/>
-          <input placeholder="confirmar senha"/>
-          <button type="submit" >Cadastrar</button>
+        <form onSubmit={ handleRegister }>
+          <input 
+            placeholder="login"
+            value={ login }
+            onChange={ e => setLogin(e.target.value) }
+          />
+          <input 
+            type="password"
+            placeholder="senha"
+            value={ password }
+            onChange={ e => setPassword(e.target.value) }
+          />
+          <input 
+            type="password"
+            placeholder="confirmar senha"
+            value={ validate }
+            onChange={ e => setValidate(e.target.value) }
+          />
+          <input 
+            type="email"
+            placeholder="email"
+            value={ email }
+            onChange={ e => setEmail(e.target.value) }
+          />
+          <input 
+            placeholder="name"
+            value={ name }
+            onChange={ e => setName(e.target.value) }
+          />
+
+          <button type="submit">Cadastrar</button>
         </form>
       </section>
     </div>
